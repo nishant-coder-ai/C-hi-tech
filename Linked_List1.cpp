92da432eb2fb97dd96106b79d47dee6d2f2d14fc
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-06-01 10:10:29 +0530

    Add files via upload

---
+++ Linked_List1.cpp
@@ -0,0 +1,75 @@
+#include<iostream>
+
+using namespace std;
+
+class Node{
+    public:
+        int data;
+        Node *next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
+    }
+
+};
+
+/// head contains address of the first Node.
+void insertAtHead(Node* &head, int val){
+    Node* n= new Node(val);
+    n->next = head;
+
+    head=n;
+}
+
+void insertAtTail(Node* &head, int val){
+
+    Node* n= new Node(val);  /// created new node n;
+    if(head==NULL){          /// check if the new node is empty , if empty then header pointer will point to new node;
+        head  = n;
+        return;
+
+    }
+
+
+    Node* temp=head;
+    while(temp->next != NULL){
+        temp= temp->next;
+    }
+    temp->next =n;
+
+}
+
+
+void display(Node* head){
+    Node* temp = head;
+    while(temp!=NULL){
+        cout<<temp->data<<" -> ";
+        temp = temp->next;
+   }
+cout<<" NULL "<<endl;
+}
+
+
+bool searchData(Node* head, int key){
+    Node* temp = head;
+    while(temp!=NULL){
+        if(temp->data==key){
+            return true;
+        }
+        temp = temp->next;
+    }
+    return false;
+}
+
+int main(){
+    Node* head = NULL;
+    insertAtTail(head, 1);
+    insertAtTail(head, 2);
+    insertAtTail(head, 3);
+    display(head);
+    insertAtHead(head, 8);
+    display(head);
+    cout<<searchData(head, 4)<<endl;
+    return 0;
+}
