aed6325b447ff2aeb83b4c93683ae46da8a14feb
Nishant <73151518+nishant-coder-ai@users.noreply.github.com>
2021-06-03 15:15:04 +0530

    Add files via upload

---
+++ Binary_Tree_Preorder.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *left;
+    struct Node *right;
+
+    Node(int val){
+            data = val;
+            left = NULL;
+            right = NULL;
+    }
+};
+
+void preorder(struct Node* root){
+    if(root==NULL){
+        return;
+    }
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+
+int main(){
+    struct Node* root = new Node(5);
+    root->left = new Node(3);
+    root->right = new Node(7);
+    root->left->left = new Node(1);
+    root->left->right = new Node(4);
+    root->right->right = new Node(8);
+    root->right->left = new Node(6);
+    /**
+                     5
+                   /   \
+                  3     7
+                 / \   / \
+                1  4   6  8
+    **/
+    preorder(root);
+
+    return 0;
+}
